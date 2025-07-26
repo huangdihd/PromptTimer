01474d0fe835cfc9345c643c802c645b1d8c6121
huangdi <hd20100104@163.com>
2025-07-26 16:43:58 +0800

    fix pug

--- PromptTimer.py
+++ PromptTimer.py
@@ -34,7 +34,7 @@ class PromptTimer(BasePlugin):
         if session_id in self.last_messages_time:
             additional_prompt += f", 距离上次对话已经过去了{time.time() - self.last_messages_time.get(session_id)}秒"
 
-        print(ctx.event.query.get_varia)
+        print(ctx.event.default_prompt)
 
     # 插件卸载时触发
     def __del__(self):
